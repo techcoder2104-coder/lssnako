d6837e6e8fdc3bb7e3767eaeafa1465f491a5f52
techcoder2104-coder <techcoder2104@gmail.com>
2026-02-07 22:58:26 +0530

    Increase request body size limit

--- server.js
+++ server.js
@@ -42,7 +42,8 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Serve uploaded files with proper CORS headers
 app.use('/uploads', (req, res, next) => {
